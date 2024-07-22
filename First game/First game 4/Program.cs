d7cffb63dd99295c486855d0e633a323af72324b
Qingque E6 <qingquestatus@outlook.com>
2024-07-22 14:13:44 +0200

    Started part 1 + 2 in First game unit 4

---
+++ First game/First game 4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information, .NET6.0
+// https://docs.google.com/document/d/1qf61PNCAHMNSIjLPgtNrMANnvBKPb6tjiuUd3iylnk4/edit
+
+
+Random rng = new Random();
+int TankDistance = rng.Next(39, 71);
+string BattlefieldStation = "_/";
+string Battlefield = "________________________________________________________________________________";
+
+while (true)
+{
+    Console.WriteLine("Enter your username: ");
+    string UserName = Console.ReadLine();
+    Console.WriteLine("Greetings " + UserName + ", its time to hunt some tanks and shoot some loads with the boys™!");
+
+    string currentBattlefield = Battlefield.Remove(TankDistance, 1).Insert(TankDistance, "T");
+    Console.WriteLine(BattlefieldStation + currentBattlefield);
+    Console.WriteLine("Heres a map of the current situation, ");
+    
+    Console.WriteLine("Aim your shot, " + UserName + ":");
+    int PlayerDistance = Convert.ToInt32(Console.ReadLine());
+
+    if (PlayerDistance < TankDistance){
+        string PlayerDestination = currentBattlefield.Remove(PlayerDistance, 1).Insert(PlayerDistance, "*");
+        Console.WriteLine(BattlefieldStation + PlayerDestination);
+        Console.WriteLine("Just too short!");
+    }
+    else if(PlayerDistance > TankDistance){
+        string PlayerDestination = currentBattlefield.Remove(PlayerDistance, 1).Insert(PlayerDistance, "*");
+        Console.WriteLine(BattlefieldStation + PlayerDestination);
+        Console.WriteLine("Bit too far!");
+    }
+    else if(PlayerDistance == TankDistance){
+        string PlayerDestination = currentBattlefield.Remove(TankDistance, 1).Insert(TankDistance, "*");
+        Console.WriteLine(BattlefieldStation + PlayerDestination);
+        Console.WriteLine("Bullseye!");   
+    }
+}
---
+++ First game/First game 4/README.md
@@ -0,0 +1,3 @@
+For Linux I had to run "dotnet run" on "~/First game 4" to make it work, 
+Visual Studio might make it easier for you though
+
