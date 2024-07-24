62ed0fd3a657266d3d30a45df7f4311f99364b0f
Qingque E6 <qingquestatus@outlook.com>
2024-07-24 12:09:16 +0200

    Finished First Game 4, Bonuspart. Might refine this though because its so much fun

--- First game/First game 4/Program.cs
+++ First game/First game 4/Program.cs
@@ -4,42 +4,49 @@ Random rng = new Random();
 int TankDistance = rng.Next(39, 71);
 string BattlefieldStation = "_/";
 string Battlefield = "________________________________________________________________________________";
-int AmountTrys = 5;
 
-    Console.WriteLine("Enter your username: ");
-    string UserName = Console.ReadLine();
-    Console.WriteLine("Greetings " + UserName + ", its time to hunt some tanks and shoot some loads with the boys™!");
+Console.WriteLine("Enter your username: ");
+string UserName = Console.ReadLine();
 
+while(TankDistance > 0){
+    Console.Clear();
+    Console.WriteLine("Heres a map of the current situation:");
     string currentBattlefield = Battlefield.Remove(TankDistance, 1).Insert(TankDistance, "T");
     Console.WriteLine(BattlefieldStation + currentBattlefield);
-    Console.WriteLine("Heres a map of the current situation, ");
-    
-for (int i = AmountTrys; i > 0 ; i--){
-    Console.WriteLine("Aim your shot, " + UserName + ". You have " + i + " bullet(s) left.");
+    Console.WriteLine("Ready your shot, " + UserName + ".");
     int PlayerDistance = Convert.ToInt32(Console.ReadLine());
 
-    if(PlayerDistance < TankDistance){
+    if(PlayerDistance < TankDistance || PlayerDistance > TankDistance){
+        TankDistance = MoveTank(TankDistance);
         string PlayerDestination = currentBattlefield.Remove(PlayerDistance, 1).Insert(PlayerDistance, "*");
         Console.WriteLine(BattlefieldStation + PlayerDestination);
-        Console.WriteLine("Just too short!");
-        GameOver(i, AmountTrys);
+
+        string DynamicReply = (PlayerDistance < TankDistance) ? "Bit too short!" : "Bit too far!";
+        Console.WriteLine(DynamicReply);
+        Console.WriteLine("Press to continue");
+        Console.ReadKey();
     }
-    else if(PlayerDistance > TankDistance){
-        string PlayerDestination = currentBattlefield.Remove(PlayerDistance, 1).Insert(PlayerDistance, "*");
-        Console.WriteLine(BattlefieldStation + PlayerDestination);
-        Console.WriteLine("Bit too far!");
-        GameOver(i, AmountTrys);
-    }
+
     else if(PlayerDistance == TankDistance){
         string PlayerDestination = currentBattlefield.Remove(TankDistance, 1).Insert(TankDistance, "*");
         Console.WriteLine(BattlefieldStation + PlayerDestination);
         Console.WriteLine("Bullseye!");   
     }
-
 }
 
-static void GameOver(int j, int TotalTrys){
-    if(j <= 1){
-        Console.WriteLine("You absolute waste of air and energy, you fumbled not just once but " + TotalTrys + " times. Get a fucking grip, you piece of shit.\nNow the tank is rolling in, killing the poor folk of your hometown. I hope you rot in hell, you disgusting pig.\nI hate you. \nI despise you.");  
-    }
+if (TankDistance <= 0)
+{
+    GameOver();
+}
+
+// Functions
+static void GameOver(){
+        Console.WriteLine("======================================================================\nYou absolute waste of air and energy, you screwed it.\nNow the tank is rolling in, killing the poor folk of your hometown.\nI hope you rot in hell, you disgusting pig.\nI despise you.\nI hate you.\n======================================================================");  
+}
+
+static int MoveTank(int TankDistance){
+    Random rng = new Random();
+    int RandomStepsForward = rng.Next(1, 16);
+    int NewTankDistance = TankDistance - RandomStepsForward;
+    return NewTankDistance;
 }
