a83b54c82d8f64362c75d2b176b0d46ff662dfc1
Qingque E6 <qingquestatus@outlook.com>
2024-08-12 14:17:46 +0200

    Finished up the Skill Increase section aswell as mission 1

---
+++ 3 - Fundamentals/1 - Lists/Mission 1/Mission-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Mission1
+{
+    class MainClass
+    {
+
+        static int ProcessStringList()
+        {
+            Random Rng = new Random();
+            int Random1 = Rng.Next(1,7);
+            int Random2 = Rng.Next(1,7);
+            int Random3 = Rng.Next(1,7);
+            int Random4 = Rng.Next(1,7);
+            int[] DiceRolls = { Random1, Random2, Random3, Random4 };
+            int Min = DiceRolls.Min();
+            int AbilityScore = DiceRolls.Sum() - Min;
+            Console.WriteLine($"You roll {Random1}, {Random2}, {Random3} and {Random4}. Your ability score is {AbilityScore}.");
+            return AbilityScore;
+        }
+        public static void Main(string[] args)
+        {
+            int AbilityScore1 = ProcessStringList();
+            int AbilityScore2 = ProcessStringList();
+            int AbilityScore3 = ProcessStringList();
+            int AbilityScore4 = ProcessStringList();
+            int AbilityScore5 = ProcessStringList();
+            int AbilityScore6 = ProcessStringList();
+        }
+    }
+}
---
+++ 3 - Fundamentals/1 - Lists/Skill-Increase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkillIncrease
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> Names = new List<string> {"Marc", "Jeremy Fragrance", "Seele", "Bakool Ja Ja's Left Nut (Fabio)"};
+            Names.Add("Bill");
+            Names.Remove("Seele");
+
+            foreach (var Name in Names)
+            {
+                Console.WriteLine(Name);
+            }
+            Console.WriteLine($"Powerlevel Tierlist:\n{Names[0]}\n{Names[2]}\n{Names[3]}\n{Names[1]}\n");
+
+        }
+    }
+}
