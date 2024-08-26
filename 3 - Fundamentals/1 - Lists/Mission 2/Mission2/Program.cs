73da383daec7e8af04542f325352e33340302a35
Qingque E6 <qingquestatus@outlook.com>
2024-08-26 12:58:34 +0200

    Finished all of Fundamentals - 1. Lists

--- 3 - Fundamentals/1 - Lists/Mission 2/Mission2/Program.cs
+++ 3 - Fundamentals/1 - Lists/Mission 2/Mission2/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Mission2
 {
     class MainClass
     {
-        static void CallToAction(string[] activeParty)
+        public static void Main(string[] args)
+        {
+            List<string> activeParty = new List<string> { "Himmel", "Guts", "Musashi", "Himura Kenshin" };
+
+            CallToAction(activeParty);
+            int EnemyHp = SpawnEnemy(8, 8);
+            InitiateCombat(activeParty, EnemyHp);
+        }
+
+        // ================= Method List =================
+
+        static void CallToAction(List<string> activeParty)
         {
             string partyMembers = string.Join(", ", activeParty); 
             Console.WriteLine($"Fighters {partyMembers} descend into the dungeon.");
@@ -23,7 +34,32 @@ namespace Mission2
             return TotalHp;
         }
 
-        static int GreatswordAttack(int AmountDice, int ValueDice)
+        static List<string> PetrifyBasilisk(List<string> activeParty)
+        {
+            if (activeParty.Count == 0)
+            {
+                return activeParty;
+            }
+            var Rng = new Random();
+            int ConstitutionModifier = 3;
+            int ConstitutionSave = Rng.Next(1, 21) + ConstitutionModifier;
+            int UpperLimit = activeParty.Count;
+            int EnemyTarget = (UpperLimit == 1) ? 0 : Rng.Next(0, UpperLimit);
+            Console.WriteLine($"The basilisk prepares its gaze-attack to attack {activeParty[EnemyTarget]}... {activeParty[EnemyTarget]} rolled a {ConstitutionSave}.");
+
+            if (ConstitutionSave < 12)
+            {
+                Console.WriteLine($"{activeParty[EnemyTarget]} got petrified!");
+                activeParty.RemoveAt(EnemyTarget);
+            }
+            else
+            {
+                Console.WriteLine($"{activeParty[EnemyTarget]} dodged the attack!");
+            }
+            return activeParty;
+        }
+
+        static int WeaponAttack(int AmountDice, int ValueDice)
         {
             var Rng = new Random();
             int TotalDmg = 0;
@@ -35,15 +71,29 @@ namespace Mission2
             return TotalDmg;
         }
 
-        static void InitiateCombat(string[] activeParty, int EnemyHp) 
+        static void GameOverScreen()
+        {
+            Console.WriteLine("======================================");
+            Console.WriteLine("You didnt vanquish the big bad evil! Yippie");
+            Console.WriteLine("======================================");
+        }
+
+        static void VictoryScreen()
+        {
+            Console.WriteLine("======================================");
+            Console.WriteLine("You vanquished the big bad evil! Yippie");
+            Console.WriteLine("======================================");
+        }
+
+        static void InitiateCombat(List<string> activeParty, int EnemyHp) 
         {
             var Rng = new Random();
 
-            while (EnemyHp > 0)
-                                                {
-                for(int i = 0; i < activeParty.Length; i++)
+            while (EnemyHp > 0 && activeParty.Count > 0)
+            {
+                for(int i = 0; i < activeParty.Count; i++)
                 {
-                    int CurrentAttack = GreatswordAttack(2, 6);
+                    int CurrentAttack = WeaponAttack(1, 4);
                     // Ternary if-else, if HP goes below 0, turn it to 0. Else just keep attacking
                     EnemyHp = (EnemyHp - CurrentAttack < 0) ? 0 : EnemyHp - CurrentAttack;
 
@@ -55,16 +105,22 @@ namespace Mission2
                         break; 
                     }
                 }
+                if (EnemyHp > 0)
+                {
+                    PetrifyBasilisk(activeParty);
+                }
+            }
+
+            if(activeParty.Count >= 0)
+            {
+                GameOverScreen();
+            }
+            else
+            {
+                VictoryScreen();
             }
         }
 
-        public static void Main(string[] args)
-        {
-            string[] activeParty = { "Himmel", "Guts", "Musashi", "Himura Kenshin" };
 
-            CallToAction(activeParty);
-            int EnemyHp = SpawnEnemy(8, 8);
-            InitiateCombat(activeParty, EnemyHp);
-        }
     }
 }
