f54ad0a7e18329f16b75dad0084326f40f918859
Qingque E6 <qingquestatus@outlook.com>
2024-09-03 13:07:38 +0200

    Fundamentals - Files - Mission 1 complete

---
+++ 3 - Fundamentals/4 - Files/Mission1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace Mission1
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            string TravellerName = null;
+            string FileName = "player-name.txt";
+
+            if (File.Exists(FileName))
+            {
+                TravellerName = File.ReadAllText("player-name.txt");
+            }
+            if (!string.IsNullOrEmpty(TravellerName))
+            {
+                WelcomeReturningPlayer(TravellerName);
+            }
+            else
+            {
+                WelcomeNewPlayer(FileName);
+
+            }
+        }
+
+        static void CreateFile(string Filename, string Text)
+        {
+            // By Default and with MonoDevelop, the file is located in bin/Debug/text.txt
+            File.WriteAllText(Filename, Text);
+        }
+
+        static void WelcomeNewPlayer(string FileName)
+        {
+            Console.WriteLine("Welcome to your biggest adventure yet!");
+            Console.WriteLine("What is your name, traveler?");
+            string TravellerName = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you, {TravellerName}!");
+            CreateFile(FileName, TravellerName);
+        }
+
+        static void WelcomeReturningPlayer(string TravellerName)
+        {
+            Console.WriteLine($"Welcome back, {TravellerName}, let's continue!");
+        }
+
+        static void DeleteFile(string Filename)
+        {
+            // deletes the first thing it can find in bin/Debug if not defined!
+            File.Delete(Filename); 
+        }       
+    }
+}
+
---
+++ 3 - Fundamentals/4 - Files/TestingGrounds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TestingGrounds
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string ExampleFile = "text.txt";
+            string ExampleFile2 = "text2.txt";
+            string ExampleString = "Hello World";
+            string ExampleString2 = "Goodbye World";
+
+            CreateFile(ExampleFile, ExampleString);
+            CreateFile(ExampleFile2, ExampleString2);
+            DeleteFile(ExampleFile);
+
+        }
+
+        static void CreateFile(string Filename, string Text)
+        {
+            // By Default and with MonoDevelop, the file is located in bin/Debug/text.txt
+            File.WriteAllText(Filename, Text);
+        }
+
+        static void DeleteFile(string Filename)
+        {
+           // deletes the first thing it can find in bin/Debug if not defined!
+            File.Delete(Filename);
+        }
+    }
+}
