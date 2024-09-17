9936db0da7f01f4ce36cb06aedfda98a3d0f4ac7
Qingque E6 <qingquestatus@outlook.com>
2024-09-17 11:24:35 +0200

    Action Adventure 1 - Mission 2 fully done

--- 4 - preparation projects/1 action-adventure/Mission2/Mission2-1/Mission2-1/Program.cs
+++ 4 - preparation projects/1 action-adventure/Mission2/Mission2-1/Mission2-1/Program.cs
@@ -17,11 +17,29 @@ class Program
         levelMatrix = new char[dimensions.Item1, dimensions.Item2];
         levelMatrix = LoadLevel();
         playerPosition = SpawnPlayer();
-        PrintLevel();
+        // PrintLevel();
+        PrintLevelWithColors();
 
 
         // =====================================================================================================================
 
+        static void SpawnTrees()
+        {
+            Random Rngesus = new Random();
+
+            for (int iy = 0; iy < 2; iy++)
+            {
+                for (int ix = 0; ix < width; ix++)
+                {
+                    int random = Rngesus.Next(1, 101);
+                    char IsItATree = (random > 71) ? '\u2660' : ' ';
+                    levelMatrix[iy, ix] = IsItATree;
+                }
+            }
+        }
+        
+        // =====================================================================================================================
+        
         static Tuple<int, int> SpawnPlayer()
         {
             for (int iy = 0; iy < levelMatrix.GetLength(0); iy++)
@@ -42,14 +60,43 @@ class Program
 
         static void PrintLevel()
         {
-            int y = levelMatrix.GetLength(0);
-            int x = levelMatrix.GetLength(1);
-
-            for (int i = 0; i < y; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < x; j++)
+                for (int j = 0; j < width; j++)
                 {
                     Console.Write(levelMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            PrintLevelDetails();
+        }
+        
+        // =====================================================================================================================
+        
+        static void PrintLevelWithColors()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    char cell = levelMatrix[y, x];
+                    switch (cell)
+                    {
+                        case '\u263a':
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case '\u2660':
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case 'M':
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        default:
+                            Console.ResetColor();
+                            break;
+                    }
+                    Console.Write(cell);
+                    Console.ResetColor(); 
                 }
                 Console.WriteLine();
             }
@@ -84,6 +131,7 @@ class Program
                 }
                 iy++;
             }
+            SpawnTrees();
             return levelMatrix; 
         }
 
@@ -92,9 +140,9 @@ class Program
         static Tuple<int, int> GetDimension(string dimensionInput)
         {
             string[] stringDimensions = dimensionInput.Split('x', 2);
-            int dimensionX = Int32.Parse(stringDimensions[0]);
-            int dimensionY = Int32.Parse(stringDimensions[1]);
-            return new Tuple<int, int>(dimensionY, dimensionX);
+            width = Int32.Parse(stringDimensions[0]);
+            height = Int32.Parse(stringDimensions[1]);
+            return new Tuple<int, int>(height, width);
         }
 
         // =====================================================================================================================
