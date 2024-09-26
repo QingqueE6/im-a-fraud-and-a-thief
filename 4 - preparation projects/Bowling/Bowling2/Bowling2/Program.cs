dd9f2aa75748cca8a105d4fe2d3cbac33f498fcd
Qingue <qingquestatus@outlook.com>
2024-09-26 13:56:50 +0200

    Bowling 2 Part 2 almost done

--- 4 - preparation projects/Bowling/Bowling2/Bowling2/Program.cs
+++ 4 - preparation projects/Bowling/Bowling2/Bowling2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static int TotalRounds = 10;
     static int[][] JechtShotMark3 = new int[TotalRounds][];
+    static int[] pointsGained = new int[TotalRounds];
+    static int[] frameScores = new int[TotalRounds];
+    
     static Random Randowiz = new Random();
     
     static void Main(string[] args)
@@ -31,6 +34,9 @@ class Program
             {
                 Console.WriteLine($"Round {i + 1}: No shots taken.");
             }
+            Console.WriteLine("Points this frame: " + pointsGained[i]);
+            Console.WriteLine("Total points: " + frameScores[i]);
+            Console.WriteLine("===============");
         }
     }
 
@@ -47,20 +53,31 @@ class Program
             {
                 StandingPins -= Shot1;
                 int Shot2 = Randowiz.Next(0, StandingPins + 1);
+                
                 JechtShotMark3[i] = new int[2]{Shot1, Shot2};
+                pointsGained[i] = Shot1 + Shot2;
                 StandingPins = 10;
             }
             else
             {
                 JechtShotMark3[i] = new int[1]{Shot1};
+                pointsGained[i] = Shot1;
                 StandingPins = 10;
             }
-            
+                
+            for (int j = i; j >= 0; j--)
+            {
+                frameScores[i] += pointsGained[j];
+            }
         }
+
+
+        
     }
 
     static void LastRound()
     {
+        int IndexLastRound = TotalRounds - 1;
         int FinalShot1 = Randowiz.Next(0, TotalRounds);
         
         int FinalShot2 = (FinalShot1 == 10) 
@@ -71,6 +88,11 @@ class Program
             ? Randowiz.Next(0, TotalRounds) 
             : Randowiz.Next(0, TotalRounds - FinalShot1);
         
-        JechtShotMark3[9] = new int[3]{FinalShot1, FinalShot2, FinalShot3};
+        JechtShotMark3[IndexLastRound] = new int[3]{FinalShot1, FinalShot2, FinalShot3};
+        pointsGained[IndexLastRound] = FinalShot1 + FinalShot2 + FinalShot3;
+        for (int i = 0; i < TotalRounds; i++)
+        {   
+            frameScores[IndexLastRound] += pointsGained[i];
+        }
     }
 }
